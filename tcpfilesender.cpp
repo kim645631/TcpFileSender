87848cb04f27e9e369fae7a28e050f625cd6c9b4
HSU,WEI-YU <41343121@nfu.edu.tw>
2025-11-27 19:40:53 +0800

    D

--- tcpfilesender.cpp
+++ tcpfilesender.cpp
@@ -17,13 +17,30 @@ TcpFileSender::TcpFileSender(QWidget *parent)
     buttonBox->addButton(startButton,QDialogButtonBox::ActionRole);
     buttonBox->addButton(openButton,QDialogButtonBox::ActionRole);
     buttonBox->addButton(quitButton,QDialogButtonBox::RejectRole);
+
+    QVBoxLayout *mainLayout = new QVBoxLayout;
+    mainLayout->addWidget(clientProgressBar);
+    mainLayout->addWidget(clientStatusLable);
+    mainLayout->addStretch(1);
+    mainLayout->addSpacing(10);
+    mainLayout->addWidget(buttonBox);
+    setLayout(mainLayout);
+    setWindowTitle(tr("檔案傳送"));
+    connect(openButton, SIGNAL(clicked()),this,SLOT(openFile()));
+    connect(startButton, SIGNAL(clicked()),this,SLOT(start()));
+    connect(&tcpClient,SIGNAL(connected()),this,SLOT(startTransfer()));
+    connect(&tcpClient,SIGNAL(bytesWritten(qint64)),this,SLOT(updateClientProgress(qint64)));
+    connect(quitButton,SIGNAL(clicked()),this,SLOT(close()));
 }
 
 TcpFileSender::~TcpFileSender() {
 
 }
 void TcpFileSender::start(){
-
+    startButton->setEnabled(false);
+    bytesWritten = 0;
+    clientStatusLable->setText(tr("連線中..."));
+    tcpClient.connectToHost(QHostAddress::LocalHost,16689);
 }
 void TcpFileSender::startTransfer(){
 
@@ -32,5 +49,6 @@ void TcpFileSender::updateClientProgress(qint64 numBytes){
 
 }
 void TcpFileSender::openFile(){
-
+    fileNane = QFileDialog::getOpenFileName(this);
+        if(!fileNane.isEmpty()) startButton->setEnabled(true);
 }
